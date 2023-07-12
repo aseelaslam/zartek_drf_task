b51b5980323fad6e85aa0830745a90dd9c79d753
Aseel Aslam <aseelaslam@gmail.com>
2023-07-12 11:47:05 +0530

    added real time ride tracking & ride matching

--- ride/views.py
+++ ride/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 
 from rest_framework import generics, permissions,status,generics
 from rest_framework.response import Response
@@ -7,7 +8,17 @@ from django.contrib.auth import authenticate
 from .models import User,Ride
 from .serializers import UserRegistrationSerializer, UserLoginSerializer,RideSerializer
 from rest_framework.views import APIView
-from rest_framework.exceptions import ValidationError
+
+
+from .tasks import update_ride_location
+
+from rest_framework.permissions import IsAuthenticated
+
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+
 
  
 
@@ -86,3 +97,69 @@ class RideCancelView(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.instance.status = 'cancelled'
         serializer.save()
+
+
+def start_ride_tracking(request, ride_id):
+    ride = get_object_or_404(Ride, id=ride_id)
+
+    update_ride_location.apply_async(args=[ride_id], countdown=5)
+    return HttpResponse("Ride tracking started")
+
+
+
+class AcceptRideView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        driver = request.user  
+        ride_id = request.data.get('ride_id')
+
+        try:
+            ride = Ride.objects.get(id=ride_id, driver=None)  
+        except Ride.DoesNotExist:
+            return Response({'message': 'Ride not found or already assigned to a driver'}, status=400)
+        
+
+
+
+
+class RideModelTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(username='user1')
+        self.user2 = User.objects.create(username='user2')
+        self.ride = Ride.objects.create(rider=self.user1, pickup_location='Location 1', dropoff_location='Location 2')
+
+    def test_ride_creation(self):
+        ride = Ride.objects.get(id=self.ride.id)
+        self.assertEqual(ride.rider, self.user1)
+        self.assertEqual(ride.pickup_location, 'Location 1')
+        self.assertEqual(ride.dropoff_location, 'Location 2')
+        self.assertIsNone(ride.driver)
+
+    def test_ride_list_api(self):
+        client = APIClient()
+        url = reverse('ride-list')
+        response = client.get(url)
+        rides = Ride.objects.all()
+        serializer = RideSerializer(rides, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+
+class AcceptRideAPITestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(username='user1')
+        self.user2 = User.objects.create(username='user2')
+        self.ride = Ride.objects.create(rider=self.user1, pickup_location='Location 1', dropoff_location='Location 2')
+
+    def test_accept_ride_api(self):
+        client = APIClient()
+        url = reverse('accept-ride')
+        data = {'ride_id': self.ride.id}
+        client.force_authenticate(user=self.user2)  # Authenticate as a driver
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], 'Ride accepted successfully')
+        self.ride.refresh_from_db()
+        self.assertEqual(self.ride.driver, self.user2)
+       
--- rideproject/settings.py
+++ rideproject/settings.py
@@ -136,3 +136,9 @@ REST_FRAMEWORK = {
     ]
     
 }
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+
